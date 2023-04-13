d7c8e55943dcf0f6fceadfd4d38ae000a4fd1713
Notnotniche <nklingsh@student.42.fr>
2023-04-13 17:56:36 +0200

    split bientot fini

--- parser/read_line.c
+++ parser/read_line.c
@@ -6,7 +6,7 @@
 /*   By: nklingsh <nklingsh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 17:05:06 by nklingsh          #+#    #+#             */
-/*   Updated: 2023/04/13 16:47:49 by nklingsh         ###   ########.fr       */
+/*   Updated: 2023/04/13 17:53:34 by nklingsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,30 +75,36 @@ int *index_in_tab(char *str)
 	int i;
 	int *tab;
 	int y;
+	int res;
 
 	i = 0;
 	y = 0;
+	res = 0;
 	tab = malloc(sizeof(int) * (number_of_letter(str) * 2));
 	if(!tab)
 		return (NULL);
 	while(str[i])
 	{
-		if(str[i] != ' ')
+		if (res == 0 && str[0] != ' ')
 		{
-			if ((str[i + 1] == ' ') || (str[i + 1] == '\0'))
-			{	
-				tab[y] = i;
-				printf("fin de mot : %d \n",i);
+			res = 12000;
+			tab[y] = i;
+			y++;
+		}
+		if (str[i] == ' ')
+		{
+			if(str[i + 1] != ' ')
+			{
+				tab[y] = i + 1;
 				y++;
 			}
 			i++;
 		}
-		if (str[i] == ' ' || str[i] == '\0')
+		if(str[i] != ' ')
 		{
-			if ((str[i + 1] != ' '))
+			if (str[i + 1] == '\0' || str[i + 1] == ' ')
 			{
 				tab[y] = i;
-				printf("debut de mot : %d \n",i);
 				y++;
 			}
 			i++;
